9060b90fde23ea827ae598429108f3e3dbb51e8a
Phasma <[ROT13] cunfzn NG ovanelpber QBG bet>
2013-01-13 19:49:42 +0000

    Major update to script - Rewritten to support multiple launchers and added launcher options for Technic Pack and Spoutcraft.

--- README.md
+++ README.md
@@ -1,36 +1,41 @@
 Minecraft Installer for Linux
 =============================
 
-Installation script for Minecraft on Linux. Tested on Ubuntu Linux 12.10 with
-Oracle JRE7.
+Installation script for Minecraft, Technic Pack, and Spoutcraft on Linux.
+Tested on Ubuntu Linux 12.10 with Oracle JRE7. Other distributions and JREs
+may or may not work. Please report any bugs and/or fixes.
 
 Functionality
 -------------
 
 ###Summary
-* Downloads Minecraft
+* Downloads Minecraft / Technic Pack / Spoutcraft
 * Creates shortcut (only tested in Unity)
 * Pins shortcut to Unity Launcher (if using Unity)
 * Uses NVIDIA Optimus if available
 
 ###Detail
-* Creates launch script for Minecraft in `~/.minecraft/minecraft.sh`
-* Adds shell alias for `minecraft`
-* Creates and pins shortcut as detailed in Summary section
+* Creates launch script for Minecraft / Technic Pack / Spoutcraft in
+  `~/.minecraft/minecraft.sh`
+* Adds shell alias for `minecraft` / `technic` / `spout`
+* Creates and pins shortcut as detailed in summary
 * Determines Java environment automatically, sets `LD_LIBRARY_PATH` in launch
   script to work around a bug that occurs when using JRE7 on 64-bit systems
 * Automatically determines memory settings to use - Defaults to initial java
   heap size of 512MB, maximum heap size of half available RAM
-* Creates logs in `~/.minecraft` which are populated when Minecraft is run
+* Creates logs in `~/.minecraft` / `~/.technicpack` / `~/.spoutcraft` which
+  are populated each time when Minecraft / Technic Pack / Spoutcraft is run
 
 Usage
 -----
 
-`./minecraftinst.sh`
+`./minecraftinst.sh [minecraft|technic|spout]`
 
-The script does not currently accept any parameters, but you can modify a number
-of parameters within the "Configuration" section of the script. The default
-values should work fine.
+The script accepts a single parameter which should dictate which launcher you
+want to install. The current options are as follows:
+* `minecraft` - Installs [Minecraft](http://minecraft.net/)
+* `technic` - Installs [Technic Pack](http://www.technicpack.net/)
+* `spout` - Installs [Spoutcraft](http://www.spout.org/)
 
 Configuration
 -------------
@@ -41,41 +46,73 @@ You should not need to edit any of these by default.
 Change to override defaults, all values are in MB.
 
 <pre>
-MIN_MEM					Minimum memory requirement (default 2048 as per docs)
-JRE_XMX					Max memory heap size (default is half of total RAM, set
-						to a non-null value to override calculation)
-JRE_XMS					Initial memory heap size (default 512)
+MIN_MEM         Minimum memory requirement (default 2048 as per docs)
+JRE_XMX         Max memory heap size (default is half of total RAM, set to a
+                non-null value to override calculation)
+JRE_XMS         Initial memory heap size (default 512)
 </pre>
 
 ###Java enviroment settings
 Change to override defaults.
 
 <pre>
-JAVA_BIN				Full path to java binary (default is worked out using
- 						`which` to locate java binary, set a custom path here
- 						to override)
-JAVA_LIB				Full path to lib folder (default is worked out from the
- 						path to `java`, set a custom path here to override)
+JAVA_BIN        Full path to java binary (default is worked out using `which`
+                to locate java binary, set a custom path here to override)
+JAVA_LIB        Full path to lib folder (default is worked out from the path
+                to `java`, set a custom path here to override)
 </pre>
 
-###Advanced script options
-Do not change these unless you understand what it is that they do.
+###NVIDIA Optimus settings
+Advanced - Change to set custom flags to use for `optirun`.
 
 <pre>
-MINECRAFT_DIR			Directory for Minecraft
-MINECRAFT_LOG			Path to STDOUT log, overwritten during each execution
-MINECRAFT_ERR_LOG		Path to STDERR log, overwritten during each execution
-MINECRAFT_JAR			Location to download/look for launcher
-MINECRAFT_SCRIPT		Path to script to launch Minecraft, launch script is
- 						created by the current script
-MINECRAFT_SHORTCUT		Path for launcher shortcut, this should always be
- 						called minecraft.desktop
-MINECRAFT_ICON			Path to download icon to and use for shortcut
-MINECRAT_ICON_URL		URL to download icon from
-OPTIRUN_FLAGS			If your system uses NVIDIA Optimus, it will by used
- 						by this script. You can specify custom flags to pass
- 						to `optirun` using this variable.
+OPTIRUN_FLAGS   If your system uses NVIDIA Optimus, it will by used by this
+                script. You can specify custom flags to pass to `optirun`
+                using this variable.
 </pre>
+
+Launchers
+---------
+The script now supports multiple launchers, as defined by the LAUNCHERS array
+and following parameter array for each launcher.
+
+###Launcher configuration
+There are two parts to defining launchers; the LAUNCHERS array, and the
+launcher-specific settings arrays.
+
+The LAUNCHERS array defines the list of available launchers - These values are
+also used as the script parameter options.
+
+The launcher-specific settings arrays must have the same name as their
+corresponding entry in the LAUNCHERS array, except that the name of the
+launcher-specific settings array must be in uppercase. The following fields
+are available for each launcher, any optional ones may be left blank by
+setting their value to `""`:
+#   Display name, base directory, JAR URL, JAR filename, shortcut filename,
+#   icon URL, custom command[s] to execute before launching.
+1. Display name - The "friendly" name for the launcher, used in places such as
+   the shortcut name. Example: `"Spoutcraft"`
+1. Base directory - Directory in which to place the JAR file for the launcher,
+   this should ideally be the same place as where the launcher stores its files.
+   Example: `"${HOME}/.spoutcraft"`
+1. JAR URL - Full address of the JAR file that should be downloaded for the
+   launcher. Example: `http://get.spout.org/Spoutcraft.jar`
+1. JAR filename - Name to save the JAR file as within the base directory.
+   Example: `spoutcraft.jar`
+1. Shortcut filename - Name to save the shortcut as, this should usually have
+   the `.desktop` extension. Example: `spoutcraft.desktop`
+1. Icon URL - Full address of the icon to download and use for the shortcut,
+   this should be in PNG format. Example: `http://i.imgur.com/xY1MG.png`
+1. Custom command - Any custom commands to run in the launcher script before
+   running launcher. Example: `touch ${HOME}/.techniclauncher/rtemp` (This
+   example prevents Technic Pack from automatically respawning - Without this
+   workaround it would not use `optirun`)
+
+###Adding new launchers
+There are two steps to adding a new launcher:
+1. Add a new entry to the LAUNCHERS array,
+1. Add a new launcher-specific settings array to define the various options
+   for installing the launcher.
 
 Licensing
 ---------
--- minecraftinst.sh
+++ minecraftinst.sh
@@ -3,10 +3,11 @@
 #  Minecraft Installer for Linux                                               #
 #  Filename    : minecraftinst.sh                                              #
 #  Author      : Phasma <phasma [AT] binarycore [DOT] org>                     #
-#  URL         : https://github.com/AtrumPhasma/minecraftinst
-#  Description : Script to install Minecraft on Linux. Tested on Ubuntu with   #
-#                Oracle JRE7. Other distributions and JREs may or may not      #
-#                work. Please report any bugs and/or fixes.                    #
+#  URL         : https://github.com/AtrumPhasma/minecraftinst                  #
+#  Description : Script to install Minecraft, Technic Pack, and Spoutcraft on  #
+#                Linux. Tested on Ubuntu with Oracle JRE7. Other distributions #
+#                and JREs may or may not work. Please report any bugs and/or   #
+#                fixes.                                                        #
 #------------------------------------------------------------------------------#
 #  Copyright 2013 Phasma <phasma [AT] binarycore [DOT] org>                    #
 #  All rights reserved.                                                        #
@@ -16,51 +17,47 @@
 #------------------------------------------------------------------------------#
 
 #- Configuration --------------------------------------------------------------#
-# Memory settings (change to override defaults)
-# Note: All values are in MB
-# VARIABLE				DESCRIPTION
-# MIN_MEM				Minimum memory requirement (default 2048 as per docs)
+# Memory settings, change to override defaults. All values are in MB.
+#  MIN_MEM          Minimum memory requirement (default 2048 as per docs)
 MIN_MEM_REQ="2048"
-# JRE_XMX				Max memory heap size (default is half of total RAM, set
-#						to a non-null value to override calculation)
+#  JRE_XMX          Max memory heap size (default is half of total RAM, set
+#                   to a non-null value to override calculation)
 JRE_XMX=""
-# JRE_XMS				Initial memory heap size (default 512)
+#  JRE_XMS          Initial memory heap size (default 512)
 JRE_XMS="512"
 
 # Java enviroment settings (change to override defaults)
-# VARIABLE				DESCRIPTION
-# JAVA_BIN				Full path to java binary (default is worked out using
-#						`which` to locate java binary, set a custom path here
-#						to override)
+#  JAVA_BIN         Full path to java binary (default is worked out using
+#                   `which` to locate java binary, set a custom path here
+#                   to override)
 JAVA_BIN=""
-# JAVA_LIB				Full path to lib folder (default is worked out from the
-#						path to `java`, set a custom path here to override)
+#  JAVA_LIB         Full path to lib folder (default is worked out from the
+#                   path to `java`, set a custom path here to override)
 JAVA_LIB=""
 
-# Advanced script options - Change with caution
-# VARIABLE				DESCRIPTION
-# MINECRAFT_DIR			Directory for Minecraft
-MINECRAFT_DIR="${HOME}/.minecraft"
-# MINECRAFT_LOG			Path to STDOUT log, overwritten during each execution
-MINECRAFT_LOG="${MINECRAFT_DIR}/minecraft.log"
-# MINECRAFT_ERR_LOG		Path to STDERR log, overwritten during each execution
-MINECRAFT_ERR_LOG="${MINECRAFT_DIR}/minecraft.err.log"
-# MINECRAFT_JAR			Location to download/look for launcher
-MINECRAFT_JAR="${MINECRAFT_DIR}/minecraft.jar"
-# MINECRAFT_SCRIPT		Path to script to launch Minecraft, launch script is
-#						created by the current script
-MINECRAFT_SCRIPT="${MINECRAFT_DIR}/minecraft.sh"
-# MINECRAFT_SHORTCUT	Path for launcher shortcut, this should always be
-#						called minecraft.desktop
-MINECRAFT_SHORTCUT="${HOME}/.local/share/applications/minecraft.desktop"
-# MINECRAFT_ICON		Path to download icon to and use for shortcut
-MINECRAFT_ICON="${MINECRAFT_DIR}/minecraft.png"
-# MINECRAT_ICON_URL		URL to download icon from
-MINECRAFT_ICON_URL="http://i.imgur.com/HmBUF.png"
-# OPTIRUN_FLAGS			If your system uses NVIDIA Optimus, it will by used
-#						by this script. You can specify custom flags to pass
-#						to `optirun` using this variable.
+# NVIDIA Optimus settings
+#  OPTIRUN_FLAGS    If your system uses NVIDIA Optimus, it will by used
+#                   by this script. You can specify custom flags to pass
+#                   to `optirun` using this variable.
 OPTIRUN_FLAGS=""
+
+#- Launchers ------------------------------------------------------------------#
+# Define supported launchers
+LAUNCHERS=("minecraft" "technic" "spout")
+
+# Define options for supported launchers, name must be uppercase version of
+# corresponding entry in LAUNCHERS array. Format is as follows:
+#   Display name, base directory, JAR URL, JAR filename, shortcut filename,
+#   icon URL, custom command[s] to execute before launching.
+MINECRAFT=("Minecraft" "${HOME}/.minecraft" "https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft.jar" "minecraft.jar" "minecraft.desktop" "http://i.imgur.com/HmBUF.png" "")
+TECHNIC=("Technic Pack" "${HOME}/.techniclauncher" "http://mirror.technicpack.net/Technic/technic-launcher.jar" "technic-launcher.jar" "techniclauncher.desktop" "http://i.imgur.com/SObea.png" "touch ${HOME}/.techniclauncher/rtemp")
+SPOUT=("Spoutcraft" "${HOME}/.spoutcraft" "http://get.spout.org/Spoutcraft.jar" "spoutcraft.jar" "spoutcraft.desktop" "http://i.imgur.com/xY1MG.png" "")
+
+# Launcher notes:
+#   Technic
+#     The custom command for technic is a workaround to allow optirun to be
+#     used, all credit for this goes to dreadslicer on the Technic Forums:
+#     http://preview.tinyurl.com/byqywsg
 
 #- Functions ------------------------------------------------------------------#
 # Precede output messages with timestamp
@@ -68,7 +65,7 @@ OPTIRUN_FLAGS=""
 # Example : F_MESSAGE "Message to output"
 F_MESSAGE()
 {
-	echo "$(date +%T) ${1}"
+    echo "$(date +%T) ${1}"
 }
 
 # Output error message before exiting
@@ -77,184 +74,210 @@ F_MESSAGE()
 # Notes   : Both parameters are optional
 F_ERR_EXIT()
 {
-	# If error message specified, output it
-	if [ -n "${1}" ]
-	then
-		F_MESSAGE "ERROR : ${1}"
-	fi
+    # If error message specified, output it
+    if [ -n "${1}" ]
+    then
+        F_MESSAGE "ERROR : ${1}"
+    fi
 
-	# If custom RC is specified and is a number then use it, otherwise exit
-	# with RC 1
-	if [ -n "${2}" ] && [ "${2}" -eq "${2}" 2>/dev/null ]
-	then
-		F_MESSAGE "EXITING : returncode ${2}"
-		exit "${2}"
-	else
-		F_MESSAGE "EXITING : returncode 1"
-		exit 1
-	fi
+    # If custom RC is specified and is a number then use it, otherwise exit
+    # with RC 1
+    if [ -n "${2}" ] && [ "${2}" -eq "${2}" 2>/dev/null ]
+    then
+        F_MESSAGE "EXITING : returncode ${2}"
+        exit "${2}"
+    else
+        F_MESSAGE "EXITING : returncode 1"
+        exit 1
+    fi
 }
 
 #- Start ----------------------------------------------------------------------#
-F_MESSAGE "Starting Minecraft installation"
+# Check parameter passed and convert to lowercase
+LAUNCHER="${1,,}"
+
+# Check if option passed is valid
+if [ "$(echo ${LAUNCHERS[*]} | grep -E -c -w "\<${LAUNCHER}\>")" -eq "1" ]
+then
+    PARAM_ARRAY_NAME="${LAUNCHER^^}[@]"
+    PARAMETERS=("${!PARAM_ARRAY_NAME}")
+else
+    F_MESSAGE "Usage : ${0} [$(echo ${LAUNCHERS[*]} | sed 's/ /|/g')]"
+    exit 1
+fi
+
+# Set parameters for chosen launcher
+DISPLAY_NAME="${PARAMETERS[0]}"
+BASE_DIR="${PARAMETERS[1]}"
+JAR_URL="${PARAMETERS[2]}"
+JAR_NAME="${PARAMETERS[3]}"
+JAR_PATH="${BASE_DIR}/${JAR_NAME}"
+SHORTCUT_FILENAME="${PARAMETERS[4]}"
+SHORTCUT_PATH="${HOME}/.local/share/applications/${SHORTCUT_FILENAME}"
+ICON_URL="${PARAMETERS[5]}"
+LAUNCH_SCRIPT="${BASE_DIR}/launch.sh"
+CUSTOM_COMMANDS="${PARAMETERS[6]}"
+
+# Start installation
+F_MESSAGE "Starting ${DISPLAY_NAME} installation"
 
 # Set java binary path using which if not overriden by user
 if [ -z ${JAVA_BIN} ]
 then
-	JAVA_BIN="$(which java)"
+    JAVA_BIN="$(which java)"
 fi
 
 # Check if we've got a java binary in our path
 if [ ! -x ${JAVA_BIN} ]
 then
-	F_ERR_EXIT "Java not found, please install before attempting to install Minecraft"
+    F_ERR_EXIT "Java not found, please install before attempting to install ${DISPLAY_NAME}"
 fi
 
 # Work out library path if not overriden by user
 if [ -z ${JAVA_LIB} ]
 then
-	# Get OS bitness and convert to lib path
-	case $(uname -m) in
-		x86_64) LIB_DIR="lib\/amd64"
-				;;
-	 i386|i686) LIB_DIR="lib"
-				;;
-			 *) F_ERR_EXIT "Unsupported architecture"
-				;;
-	esac
-	# Get LIB path by:
-	#  1) Reading symlinks to get actual path to java binary (readlink)
-	#  2) Getting full directory path that java binary is in (dirname)
-	#  3) Replacing "bin" with LIB_DIR as set above (sed)
-	JAVA_LIB="$(ls -d $(dirname $(readlink -f ${JAVA_BIN})|sed "s/bin$/${LIB_DIR}/"))"
+    # Get OS bitness and convert to lib path
+    case $(uname -m) in
+        x86_64) LIB_DIR="lib\/amd64"
+                ;;
+     i386|i686) LIB_DIR="lib"
+                ;;
+             *) F_ERR_EXIT "Unsupported architecture"
+                ;;
+    esac
+    # Get LIB path by:
+    #  1) Reading symlinks to get actual path to java binary (readlink)
+    #  2) Getting full directory path that java binary is in (dirname)
+    #  3) Replacing "bin" with LIB_DIR as set above (sed)
+    JAVA_LIB="$(ls -d $(dirname $(readlink -f ${JAVA_BIN})|sed "s/bin$/${LIB_DIR}/"))"
 fi
 
 # Check if java library directory is valid, this is needed to get jre7
 # to work correctly on 64-bit systems
 if [ ! -d ${JAVA_LIB} ]
 then
-	F_ERR_EXIT "Unable to locate library path for java"
+    F_ERR_EXIT "Unable to locate library path for java"
 fi
 
 # Check memory and calculate max (if not overridden)
 if [ -z ${JRE_XMX} ]
 then
-	# If total_ram>=MIN_MEM returns total_ram/2, otherwise returns zero
-	JRE_XMX="$(free -m | awk -v min_mem=${MIN_MEM} '$1 ~ /^Mem/ {if($2>=min_mem) print $2/2; else print 0}')"
-	if [ "${JRE_XMX}" -eq "0" ]
-	then
-		F_ERR_EXIT "Insufficient RAM, less than MIN_MEM value of ${MIN_MEM}"
-	fi
+    # If total_ram>=MIN_MEM returns total_ram/2, otherwise returns zero
+    JRE_XMX="$(free -m | awk -v min_mem=${MIN_MEM} '$1 ~ /^Mem/ {if($2>=min_mem) print $2/2; else print 0}')"
+    if [ "${JRE_XMX}" -eq "0" ]
+    then
+        F_ERR_EXIT "Insufficient RAM, less than MIN_MEM value of ${MIN_MEM}"
+    fi
 fi
 
-# Check if we've got minecraft.jar, download if we haven't
-if [ ! -f ${MINECRAFT_JAR} ]
+# Check if we've got jar, download if we haven't
+if [ ! -f ${JAR_PATH} ]
 then
-	F_MESSAGE "Downloading minecraft.jar..."
-	mkdir -p ${MINECRAFT_DIR} || F_ERR_EXIT "Unable to create ${MINECRAFT_DIR}"
-	# Get URL to minecraft.jar
-	MINECRAFT_JAR_URL="$(wget -qO- http://minecraft.net/download | sed -n 's/.*"\([a-zA-Z0-9:./].*minecraft.jar\)".*/\1/p')"
-	# Download minecraft.jar
-	wget -nv -P ${MINECRAFT_DIR} ${MINECRAFT_JAR_URL} || F_ERR_EXIT "Unable to download minecraft.jar, please check connectivity and try again later"
+    F_MESSAGE "Downloading ${JAR_NAME}..."
+    mkdir -p ${BASE_DIR} || F_ERR_EXIT "Unable to create ${BASE_DIR}"
+    # Download jar file
+    wget -nv -O ${JAR_PATH} ${JAR_URL} || F_ERR_EXIT "Unable to download ${JAR_NAME}, please check connectivity and try again later"
 fi
 
 # Check if we're on a system that uses NVIDIA Optimus
 if [ -x $(which optirun) ]
 then
-	# Needs a trailing space
-	OPTIRUN_CMD="$(which optirun) ${OPTIRUN_FLAGS} "
+    # Needs a trailing space
+    OPTIRUN_CMD="$(which optirun) ${OPTIRUN_FLAGS} "
 else
-	OPTIRUN_CMD=""
+    OPTIRUN_CMD=""
 fi
 
-# Minecraft launch command
-MINECRAFT_RUN="${OPTIRUN_CMD}${JAVA_BIN} -Xmx${JRE_XMX}M -Xms${JRE_XMS}M -cp ${MINECRAFT_JAR} net.minecraft.LauncherFrame 1>${MINECRAFT_LOG} 2>${MINECRAFT_ERR_LOG}"
+# Generate launch command
+LAUNCHER_RUN="${OPTIRUN_CMD}${JAVA_BIN} -Xmx${JRE_XMX}M -Xms${JRE_XMS}M -jar ${JAR_PATH} 1>${BASE_DIR}/run.log 2>${BASE_DIR}/error.log"
 
 # Create launcher script
-tee<<EOF>${MINECRAFT_SCRIPT}
+tee<<EOF>${LAUNCH_SCRIPT}
 #!/bin/bash
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${JAVA_LIB}
-${MINECRAFT_RUN}
+${CUSTOM_COMMANDS}
+${LAUNCHER_RUN}
 EOF
 # Set permissions
-chmod u+x ${MINECRAFT_SCRIPT}
+chmod u+x ${LAUNCH_SCRIPT}
 
 # Create shortcut, remove if applicable
 # 1. Download icon if we don't have it
-if [ ! -f ${MINECRAFT_ICON} ]
+LAUNCHER_ICON="${BASE_DIR}/icon.png"
+if [ ! -f ${LAUNCHER_ICON} ]
 then
-	F_MESSAGE "Downloading Minecraft icon..."
-	# Attempt to download icon, delete dummy file and exit with error if fails
-	wget -nv -O ${MINECRAFT_ICON} ${MINECRAFT_ICON_URL} || F_ERR_EXIT "$(rm -f ${MINECRAFT_ICON} 1>/dev/null 2>&1) Unable to download Minecraft icon, please check connectivity and try again later"
+    F_MESSAGE "Downloading icon for ${DISPLAY_NAME}..."
+    # Attempt to download icon, delete dummy file and exit with error if fails
+    wget -nv -O ${LAUNCHER_ICON} ${ICON_URL} || F_ERR_EXIT "$(rm -f ${LAUNCHER_ICON} 1>/dev/null 2>&1) Unable to download ${DISPLAY_NAME} icon, please check connectivity and try again later"
 fi
 
 # 2. Unpin first (if Unity is in use)
 if [ "$(ps -ef | awk '$NF ~ /unity-panel-service$/ {print 1}')" -eq "1" ]
 then
-	# Check if shortcut is currently pinned
-	if [ "$(gsettings get com.canonical.Unity.Launcher favorites|grep -c "minecraft.desktop")" -gt "0" ]
-	then
-		# If it is, unpin it
-		F_MESSAGE "Unpinning current shortcut from Launcher"
-		gsettings set com.canonical.Unity.Launcher favorites "$(gsettings get com.canonical.Unity.Launcher favorites|sed "s%, 'application://minecraft.desktop'%%g")"
-	fi
+    # Check if shortcut is currently pinned
+    if [ "$(gsettings get com.canonical.Unity.Launcher favorites|grep -c "${SHORTCUT_FILENAME}")" -gt "0" ]
+    then
+        # If it is, unpin it
+        F_MESSAGE "Unpinning current shortcut from Launcher"
+        gsettings set com.canonical.Unity.Launcher favorites "$(gsettings get com.canonical.Unity.Launcher favorites|sed "s%, 'application://${SHORTCUT_FILENAME}'%%g")"
+    fi
 fi
 
 # 3. Delete existing shortcut (if exists)
-if [ -f ${MINECRAFT_SHORTCUT} ]
+if [ -f ${SHORTCUT_PATH} ]
 then
-	F_MESSAGE "Deleting current shortcut"
-	/bin/rm -f ${MINECRAFT_SHORTCUT} 1>/dev/null 2>&1
-	sleep 1
+    F_MESSAGE "Deleting current shortcut"
+    /bin/rm -f ${SHORTCUT_PATH} 1>/dev/null 2>&1
+    sleep 1
 fi
 
 # 4. Add alias to RC file if file is writable
 RCFILE="${HOME}/.bashrc"
 if [ -w ${RCFILE} ]
 then
-	# Check if alias already in place
-	if [ "$(grep -c "alias minecraft=" ${RCFILE})" -eq "0" ]
-	then
-		# No alias in place, creating...
-		F_MESSAGE "Creating \"minecraft\" shell alias"
-		tee<<EOF>>${RCFILE}
+    # Check if alias already in place
+    if [ "$(grep -c "alias ${LAUNCHER}=" ${RCFILE})" -eq "0" ]
+    then
+        # No alias in place, creating...
+        F_MESSAGE "Creating \"${LAUNCHER}\" shell alias"
+        tee<<EOF>>${RCFILE}
 
-# Alias to Minecraft launch script, added by minecraftinst
-alias minecraft='${MINECRAFT_SCRIPT} &'
+# Alias to ${DISPLAY_NAME} launch script, added by Minecraft Installer for Linux
+alias ${LAUNCHER}='${LAUNCH_SCRIPT} &'
 EOF
-	else
-		F_MESSAGE "\"minecraft\" alias shell exists, skipping"
-	fi
+    else
+        F_MESSAGE "\"${LAUNCHER}\" alias shell exists, skipping"
+    fi
 fi
 
 # 5. Create shortcut
 F_MESSAGE "Creating shortcut"
 
 # Make shortcut folder if it doesn't exist
-MINECRAFT_SHORTCUT_DIR="$(dirname ${MINECRAFT_SHORTCUT})"
-mkdir -p ${MINECRAFT_SHORTCUT_DIR} || F_ERR_EXIT "Unable to create ${MINECRAFT_SHORTCUT_DIR}"
+SHORTCUT_DIR="$(dirname ${SHORTCUT_PATH})"
+mkdir -p ${SHORTCUT_DIR} || F_ERR_EXIT "Unable to create ${SHORTCUT_DIR}"
 
 # Create shortcut file
-tee<<EOF>${MINECRAFT_SHORTCUT}
+tee<<EOF>${SHORTCUT_PATH}
 [Desktop Entry]
 Version=1.0
-Name=Minecraft
-Exec=${MINECRAFT_SCRIPT}
-Icon=${MINECRAFT_ICON}
+Name=${DISPLAY_NAME}
+Exec=${LAUNCH_SCRIPT}
+Icon=${LAUNCHER_ICON}
 Terminal=false
 Type=Application
 Categories=Game;
 EOF
 
 # Grant user execute on shortcut
-chmod u+x ${MINECRAFT_SHORTCUT}
+chmod u+x ${SHORTCUT_PATH}
 
 # 6. Pin shortcut (if Unity is in use)
 if [ "$(ps -ef | awk '$NF ~ /unity-panel-service$/ {print 1}')" -eq "1" ]
 then
-	# Pin to bar
-	F_MESSAGE "Pinning shortcut to Launcher"
-	gsettings set com.canonical.Unity.Launcher favorites "$(gsettings get com.canonical.Unity.Launcher favorites|sed "s%]%, 'application://minecraft.desktop']%")"
+    # Pin to bar
+    F_MESSAGE "Pinning shortcut to Launcher"
+    gsettings set com.canonical.Unity.Launcher favorites "$(gsettings get com.canonical.Unity.Launcher favorites|sed "s%]%, 'application://${SHORTCUT_FILENAME}']%")"
 fi
 
 F_MESSAGE "Completed"
